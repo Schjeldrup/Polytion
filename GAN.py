03eb7107f947909d36a7b4ceae0ca7d54feacc61
Schjeldrup <martin@riddler.dk>
2020-04-12 15:25:29 +0200

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -2,20 +2,18 @@
 import torch.nn as nn
 import numpy as np
 import torch
-import tntorch as tn
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
-import Polytion.py
-import prepData.py
+import prepData.py as prep
 torch.set_default_dtype(torch.float64)
 from torch.utils.data import DataLoader
-
+import PolytionLayers.py as PL
 
 """
 Loader
 """
 
-images=load_images_from_folder('Images_png_01')
+images=prep.load_images_from_folder('Images_png_01')
 
 """
 downscaler
@@ -25,11 +23,14 @@ batch_size = 1
 lowres_loader = DataLoader(images, batch_size=batch_size)#, pin_memory=cuda)
 real_data_loader = DataLoader(images,batch_size=batch_size)
 
+lr_dim=128*128
+hr_dim=512*512
+
 """
 GAN Structure
 """
 
-
+my_generator = PL.Generator()
   
         
         
@@ -40,49 +41,44 @@ class discriminator(nn.Module):
         super(discriminator, self).__init__()
         # On the {z_s,d}
         self.disc = nn.Sequential(
-        nn.Conv2d(x, x, kernel_size=3, stride=1,padding=0),
+        nn.Conv2d(lr_dim+hr_dim, (lr_dim+hr_dim), kernel_size=3, stride=1,padding=0),
         nn.ReLU(),
-        nn.Conv2d(x, y, kernel_size=4, stride=2,padding=0),
-        nn.ReLU(),
-        
-        nn.Conv2d(y, y, kernel_size=3, stride=1,padding=0),
-        nn.ReLU(),
-        nn.Conv2d(y, z, kernel_size=4, stride=2,padding=0),
+        nn.Conv2d((lr_dim+hr_dim), (lr_dim+hr_dim)*2, kernel_size=4, stride=2,padding=0),
         nn.ReLU(),
         
-        nn.Conv2d(z, z, kernel_size=3, stride=1,padding=0),
+        nn.Conv2d((lr_dim+hr_dim)*2, (lr_dim+hr_dim)*2, kernel_size=3, stride=1,padding=0),
         nn.ReLU(),
-        nn.Conv2d(z, q, kernel_size=4, stride=2,padding=0),
+        nn.Conv2d((lr_dim+hr_dim)*2, (lr_dim+hr_dim)*4, kernel_size=4, stride=2,padding=0),
         nn.ReLU(),
         
-        nn.Conv2d(q, q, kernel_size=3, stride=1,padding=0),
+        nn.Conv2d((lr_dim+hr_dim)*4, (lr_dim+hr_dim)*4, kernel_size=3, stride=1,padding=0),
+        nn.ReLU(),
+        nn.Conv2d((lr_dim+hr_dim)*4, (lr_dim+hr_dim)*8, kernel_size=4, stride=2,padding=0),
+        nn.ReLU(),
+        
+        nn.Conv2d((lr_dim+hr_dim)*8, (lr_dim+hr_dim)*8, kernel_size=3, stride=1,padding=0),
         nn.ReLU())
 
     def forward(self, lowres,highres):
         combine= torch.cat([lowres,highres], dim=1)
         return self.disc(combine)
         
-my_generator = generator()
 my_discriminator = discriminator()
 
 print(my_generator.parameters)
-
-
-from torch.autograd import Variable
-loss = nn.BCELoss()  
 
 """    
 gpu_generator = generator().to(device)
 gpu_discriminator = discriminator().to(device)
 """
 
-#Declare Parameters
-loss = nn.BCELoss()
+#Declare Loss
+loss = nn.BCELoss() #We should truy other loss-functions
 batch_size = 1
 
 #Learning rate
-generator_optim = torch.optim.Adam(generator(), 2e-4, betas=(0.5, 0.9))
-discriminator_optim = torch.optim.Adam(discriminator(), 2e-4, betas=(0.5, 0.9))
+generator_optim = torch.optim.Adam(my_generator(), 2e-4, betas=(0.5, 0.9))
+discriminator_optim = torch.optim.Adam(my_discriminator(), 2e-4, betas=(0.5, 0.9))
 
 lr_scheduler = np.linspace(0,2e-4,30)
 
@@ -98,16 +94,16 @@ for epoch in range(num_epochs):
     if epoch in range(30):
         generator_optim = torch.optim.Adam(my_generator.parameters(), lr_scheduler[epoch], betas=(0.5, 0.9))
         discriminator_optim = torch.optim.Adam(my_discriminator.parameters(), lr_scheduler[epoch], betas=(0.5, 0.9))
-    for i, data in enumerate(zip(real_data_loader,attribute_loader)):
-        (split, into, parts) = data
+    for i, data in enumerate(zip(real_data_loader,lowres_loader)):
+        (split1, split2) = data
         true_label = torch.ones(batch_size, 1)#.to(device)
         fake_label = torch.zeros(batch_size, 1)#.to(device)
-        discriminator.zero_grad()
-        generator.zero_grad()
+        my_discriminator.zero_grad()
+        my_generator.zero_grad()
         
         # Step 1. Send real data through discriminator and backpropagate its errors.
-        x_true = Variable(real_data)#.to(device)
-        output = discriminator(x_true)
+        x_true = Variable(highres)#.to(device)
+        output = my_discriminator(x_true)
         
         error_true = loss(output, true_label)
         error_true.backward()
@@ -116,13 +112,13 @@ for epoch in range(num_epochs):
         noise = torch.randn(batch_size, 100, 1,1)
         noise = Variable(noise, requires_grad=False).to(device)
         
-        x_fake = generator(var)
+        x_fake = PL.Generator(lowres)
         
         # Step 3. Send fake data through discriminator
         #         propagate error and update D weights.
         # --------------------------------------------
         # Note: detach() is used to avoid updating generator gradients
-        output = discriminator(x_fake.detach(),downsampled,attributes) 
+        output = discriminator(x_fake.detach(),highres,lowres) 
         
         error_fake = loss(output, fake_label)
         error_fake.backward()
