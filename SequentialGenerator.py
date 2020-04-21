965617cb912964829cd47a4dcb1498643b5e311a
Simon Aertssen <Simonaertssen@gmail.com>
2020-04-21 10:25:12 +0200

    Sequential version working

--- SequentialGenerator.py
+++ SequentialGenerator.py
@@ -1,10 +1,5 @@
 import torch
-import threading
-import logging
 
-logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
-
-# Sequential version:
 class FTT_Layer(torch.nn.Module):
     def __init__(self, N, rank, imwidth, imheight, verbose = 0):
         super(FTT_Layer, self).__init__()
@@ -17,7 +12,6 @@ class FTT_Layer(torch.nn.Module):
         self.rank = rank
         self.s = imwidth * imheight
         self.verbose = verbose
-        self.lock = threading.Lock()
 
         # Make a list of TTcore ranks, starting from r_0 = r_N = 1: perhaps feed it in as a list? isinstance(rank, list)
         self.ranklist = [1, 1]
@@ -32,55 +26,47 @@ class FTT_Layer(torch.nn.Module):
             # Make tensors of size (r_{k-1}, n_{k} = self.s, r_{k})
             TTcore = torch.empty(self.ranklist[n], self.s, self.ranklist[n+1])
             torch.nn.init.xavier_normal_(TTcore)
+            #torch.nn.init.xavier_uniform_(TTcore)
             TTcore /= self.s
             self.TT.append(torch.nn.Parameter(TTcore))
 
-
-    def parallelVecProd(self, k):
-        d1, d2 = self.ranklist[k], self.ranklist[k+1]
-        cop = self.TT[k].clone()
-        tmp = torch.matmul(self.z, cop.permute(1,0,2).reshape(self.s, d1*d2))
-        #self.V[k,0:d1,0:d2] = tmp.reshape(d1, d2)
-        ####self.V[k] = tmp.reshape(d1, d2)
-        #with self.lock:
-        #    print('before: shape =', self.V[k,0:d1,0:d2].shape, 'version = ', self.V[k,0:d1,0:d2]._version)
-
-        with torch.no_grad():
-            self.V[k,0:d1,0:d2] = tmp.reshape(d1, d2)
-        #with self.lock:
-        #    print('after: shape =', self.V[k,0:d1,0:d2].shape, 'version = ', self.V[k,0:d1,0:d2]._version)
-
-        return
-
     def forward(self, z):
-        # Compute the forward pass: the nmode multiplications f = A x1 z x2 z x3 ··· x(N-1) z
-        # Follow algorithm 1: allocate space and compute each V^(k), possible in parallel with threads.
-        # V^(0) will not be used as it is unnecessary to compute it, it's just self.TT[0][0,:,:]
         self.z = z.clone()
 
         f = self.TT[0][0]
         for k in range(1, self.N):
             d1, d2 = self.ranklist[k], self.ranklist[k+1]
+
             extra = torch.matmul(self.z, self.TT[k].permute(1,0,2).reshape(self.s, d1*d2))
             extra = extra.reshape(d1, d2)
             f = torch.matmul(f, extra)
-        return f.reshape(-1)
+
+        out = f.reshape(-1)
+        return out
 
 
-class Generator(torch.nn.Module):
-    def __init__(self, layer, batch_size, N, rank, imwidth, imheight):
+class PolyNet(torch.nn.Module):
+    def __init__(self, layer, N, rank, imwidth, imheight):
         super(PolyNet, self).__init__()
 
-        self.batch_size = batch_size
         self.c = 1
         self.imwidth, self.imheight = imwidth, imheight
         self.s = imwidth*imheight
         self.PolyLayer = layer(N, rank, imwidth, imheight, 0)
-        self.SM = torch.nn.Sigmoid()
+        self.BN = nn.BatchNorm2d(num_features=1)
 
     def forward(self, x):
-        # Register x as attribute for parallel access
-        self.x = x.clone()
+        # Register dimensions:
+        xshape = x.shape
+        if len(xshape) == 2:
+            self.batch_size = 1
+        elif len(xshape) == 4:
+            self.batch_size = xshape[0]
+        else:
+            raise ValueError
+
+        # Register x as attribute for parallel access, and clone because dataset would be overwritten
+        self.x = self.BN(x.clone())
 
         #print('x.shape = ', x.shape)
         self.x = self.x.reshape(self.batch_size, self.c, self.s) # flatten to the 1D equivalent vector
@@ -89,4 +75,4 @@ class Generator(torch.nn.Module):
             self.x[batch, self.c-1, :] = self.PolyLayer(self.x[batch, self.c-1, :])
 
         self.x = self.x.reshape(self.batch_size, self.c, self.imwidth, self.imheight)
-        return self.x #self.SM(self.x)
+        return self.x
